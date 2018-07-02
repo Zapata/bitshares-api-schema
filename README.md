d45bfb4f9b306c72e7daa99f0ac3be24a94f72a3
Zapata <marco.tessari@gmail.com>
2018-07-02 03:04:51 +0200

    First draft.

---
+++ README.md
@@ -0,0 +1,39 @@
+Bitshares API Documentation
+-----------
+
+An attempt to formalize Bitshares RPC API definition, in order to generate documentation and provide a contract first aproach.
+
+See discussion at [bitshares-core#97](https://github.com/bitshares/bitshares-core/issues/792).
+
+Use [mzernetsch/jrgen](https://github.com/mzernetsch/jrgen) tool.
+A dedicated plugin has been written to handle: 
+- [tuples in array types](https://spacetelescope.github.io/understanding-json-schema/reference/array.html?highlight=array%20tuples#tuple-validation)
+- customizable default url (see schemas.info.defaultUrl) 
+
+# Run:
+
+```
+npm install -g mzernetsch/jrgen jrgen-plugin-bitshares
+jrgen docs/html ./BitsharesAPI.schema.json
+
+```
+Then open the generated `BitsharesAPI.html` in your favorite browser.
+
+Or Use docker:
+
+```
+docker build . -t bitshares-api-doc
+docker run bitshares-api-doc
+```
+
+# CORS:
+
+In order to be able to use the testing features on remote RPC API servers CORS should be activated.
+See [how to enable CORS](https://enable-cors.org/) on server side. Node admin will need a front hand server as Bitshares core does not support it currently.
+
+# TODO:
+
+- Use definitions and $ref to factorize schema (see [reuse in documentation](https://spacetelescope.github.io/understanding-json-schema/structuring.html?highlight=one#reuse))
+- Update Jrgen to handle `allOf`, `anyOf`, `` (see [documentation](https://spacetelescope.github.io/understanding-json-schema/reference/combining.html?highlight=allof#allof))
+- Improve Bitshares api methods coverage.
+- Document returned objects more precisely.
---
+++ jrgen-plugin-bitshares/generators/docs/html/utils.js
@@ -0,0 +1,207 @@
+const fs = require("fs-extra");
+const path = require("path");
+const $RefParser = require("json-schema-ref-parser");
+const jsonlint = require("jsonlint");
+const merge = require("deepmerge");
+const prettier = require("prettier");
+
+exports.findGenerators = generatorId => {
+  var generators = {};
+
+  var loadPlugin = pluginFolderPath => {
+    var generatorsFolderPath = path.join(pluginFolderPath, "generators");
+    fs.readdirSync(generatorsFolderPath).forEach(categoryName => {
+      var categoryFolderPath = path.join(generatorsFolderPath, categoryName);
+      fs.readdirSync(categoryFolderPath).forEach(generatorName => {
+        var generatorPath = path.join(
+          categoryFolderPath,
+          generatorName,
+          "generator.js"
+        );
+        if (fs.existsSync(generatorPath)) {
+          generators[categoryName + "/" + generatorName] = generatorPath;
+        }
+      });
+    });
+  };
+
+  var parsePlugins = pluginsFolderPath => {
+    fs.readdirSync(pluginsFolderPath).forEach(pluginName => {
+      var pluginFolderPath = path.join(pluginsFolderPath, pluginName);
+      if (pluginName.startsWith("jrgen-plugin-")) {
+        loadPlugin(pluginFolderPath);
+      } else if (pluginName.startsWith("@")) {
+        parsePlugins(pluginFolderPath);
+      }
+    });
+  };
+
+  loadPlugin(__dirname);
+  parsePlugins(path.join(__dirname, ".."));
+
+  return generators;
+};
+
+exports.populateTemplate = (template, values) => {
+  var text = new String(template);
+
+  Object.keys(values).forEach(key => {
+    var regex = new RegExp("{{" + key + "}}", "g");
+    text = text.replace(regex, values[key]);
+  });
+
+  return text;
+};
+
+exports.generateRequestExample = (method, paramsSchema) => {
+  var example = exports.generateExample(paramsSchema);
+
+  var exampleRequest = {
+    jsonrpc: "2.0",
+    id: "1234567890",
+    method: method,
+    params: example === null ? undefined : example
+  };
+
+  return JSON.stringify(exampleRequest, null, 4);
+};
+
+exports.generateResponseExample = resultSchema => {
+  var example = exports.generateExample(resultSchema);
+
+  var exampleResponse = {
+    jsonrpc: "2.0",
+    id: "1234567890",
+    result: example === null ? undefined : example
+  };
+
+  return JSON.stringify(exampleResponse, null, 4);
+};
+
+exports.generateExample = schema => {
+  if (!schema) {
+    return;
+  }
+
+  var example = null;
+
+  if (schema.type === "object") {
+    example = {};
+
+    Object.keys(schema.properties).forEach(key => {
+      example[key] = exports.generateExample(schema.properties[key]);
+    });
+  } else if (schema.type === "array") {
+    if (schema.default !== undefined || schema.example !== undefined) {
+      example = schema.default === undefined ? schema.example : schema.default;      
+    } else if (Array.isArray(schema.items)) {
+      example = schema.items.map(item => exports.generateExample(item));
+    } else {
+      example = [exports.generateExample(schema.items)];
+    }
+  } else {
+    example = schema.default === undefined ? schema.example : schema.default;
+  }
+
+  return example;
+};
+
+exports.resolveSchemaRefs = schema => {
+  var data;
+  var done = false;
+
+  $RefParser.dereference(schema, (error, schema) => {
+    if (error) {
+      throw error;
+    }
+    data = schema;
+    done = true;
+  });
+
+  require("deasync").loopWhile(() => {
+    return !done;
+  });
+
+  return data;
+};
+
+exports.loadSchema = schemaPath => {
+  if (!path.isAbsolute(schemaPath)) {
+    schemaPath = path.join(process.cwd(), schemaPath);
+  }
+
+  var jsonString = fs.readFileSync(schemaPath, {
+    encoding: "utf8"
+  });
+
+  if (!jsonString) {
+    throw "Error occured during loading schema.";
+  }
+
+  var schema = jsonlint.parse(jsonString);
+  if (!schema) {
+    throw "Specified schema does not exist.";
+  }
+
+  var resolvedSchema = exports.resolveSchemaRefs(schema);
+  if (!resolvedSchema) {
+    throw "Error occured during resolving schema $refs.";
+  }
+
+  return resolvedSchema;
+};
+
+exports.loadSchemas = schemaPaths => {
+  var schemas = [];
+
+  schemaPaths.forEach(schemaPath => {
+    schemas.push(exports.loadSchema(schemaPath));
+  });
+
+  return schemas;
+};
+
+exports.mergeSchemas = schemas => {
+  var apiSchema = {};
+
+  schemas.forEach(schema => {
+    apiSchema = merge(apiSchema, schema);
+  });
+
+  return apiSchema;
+};
+
+exports.writeArtifacts = (artifacts, outdir) => {
+  Object.keys(artifacts).forEach(artifactPath => {
+    fs.outputFile(path.join(outdir, artifactPath), artifacts[artifactPath]);
+  });
+};
+
+exports.prettifyArtifacts = artifacts => {
+  Object.keys(artifacts).forEach(key => {
+    var parser;
+    if (key.endsWith(".js")) {
+      parser = "babylon";
+    }
+    if (key.endsWith(".ts")) {
+      parser = "typescript";
+    }
+    if (key.endsWith(".css") || key.endsWith(".scss")) {
+      parser = "postcss";
+    }
+    if (key.endsWith(".json")) {
+      parser = "json";
+    }
+    if (key.endsWith(".md")) {
+      parser = "markdown";
+    }
+    if (parser) {
+      artifacts[key] = Buffer.from(
+        prettier.format(artifacts[key].toString(), {
+          parser
+        }),
+        "utf-8"
+      );
+    }
+  });
+};
